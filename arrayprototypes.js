3603afac66d0038b6956ef7751a61c87c2cc23f6
David Aaron <davidmatthewaaron@gmail.com>
2014-07-20 21:57:14 -0400

    finally resolves duplication in .sample()

--- arrayprototypes.js
+++ arrayprototypes.js
@@ -64,10 +64,10 @@ Array.prototype.sample = function (ran) {
     var indexes = [];
     var new_array = [];
     if (ran !== undefined) {
-    for (var i = 0; i < ran; i++){
+    for (var i = 0; i < ran || indexes.length < ran ; i++){
         var ran_index = Math.floor(Math.random()*len);
         var index_value = a[ran_index];
-        if (indexes.indexOf(index_value) === -1) {
+        if (indexes.indexOf(ran_index) === -1) {
         indexes.push(ran_index);
         new_array.push(index_value);
         console.log(indexes);
